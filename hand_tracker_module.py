cf026a4669305363e2b3c43d4e7b19227f9196c2
Fr0st-By7e <mrithulabejay@gmail.com>
2025-12-01 15:41:21 +0530

    2 new functions in module

--- hand_tracker_module.py
+++ hand_tracker_module.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import numpy as np
 
 class HandTracker:
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
@@ -25,19 +26,51 @@ class HandTracker:
         return img
     
     def findPosition(self, img, handNo=0, draw=True):
-        lmList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 # Hand that is being tracked is highlighted with circles on landmarks
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-        return lmList
+        return self.lmList
+    
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[4][1] > self.lmList[3][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        # Fingers
+        for id in range(1, 5):
+            if self.lmList[id * 4 + 4][2] < self.lmList[id * 4 + 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+    
+    def findDistance(self, p1, p2, img=None, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
+        if img is not None:
+            if draw:
+                cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+                cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+                cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+                cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+            length = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+            return length, img, [x1, y1, x2, y2, cx, cy]
+
+
+
+# Dummy test code
 def main():
     cap = cv2.VideoCapture(0)
     tracker = HandTracker()
